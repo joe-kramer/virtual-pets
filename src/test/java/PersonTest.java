a34d44f165135b35766648dd14c4e81ec48b87ae
Joe Kramer <joekramer1125@gmail.com>
2017-07-17 09:06:17 -0700

    add initial getter methods, attributes, and override equals for Person class

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,38 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+  @Test
+  public void person_instantiatesCorrectly_true() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    assertEquals(true, testPerson instanceof Person);
+  }
+
+  @Test
+  public void getName_personInstantiatesWithName_Henry() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    assertEquals("Henry", testPerson.getName());
+  }
+
+  @Test
+  public void getEmail_personInstantiatesWithEmail_String() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    assertEquals("henry@henry.com", testPerson.getEmail());
+  }
+
+  @Test
+  public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+    Person firstPerson = new Person("Henry", "henry@henry.com");
+    Person anotherPerson = new Person("Henry", "henry@henry.com");
+    assertTrue(firstPerson.equals(anotherPerson));
+  }
+
+  @Test
+  public void save_insertsObjectIntoDatabase_Person() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    assertTrue(Person.all().get(0).equals(testPerson));
+  }
+
+}
